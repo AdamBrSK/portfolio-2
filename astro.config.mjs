9c206521257e7209cbb6fbb64844fecc0ec20f8d
adamSk65 <adambrsk7@gmail.com>
2026-04-12 22:49:48 +0200

    upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import react from '@astrojs/react';
 
+// @ts-ignore
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+// @ts-ignore
+const repoName = process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '';
+// @ts-ignore
+const GITHUB_REPOSITORY_OWNER = process.env.GITHUB_REPOSITORY_OWNER
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  env: {
+    schema: {
+            API_URL: envField.string({ context: "client", access: "public", optional: true }),
+
+    }
+  },
+  integrations: [react()],
+  site: isGitHubPages ? `https://${GITHUB_REPOSITORY_OWNER}.github.io` : "https://web.com",
+  base: isGitHubPages ? repoName : "/",
 });
